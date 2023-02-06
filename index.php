6c2fe0059843d91b41b17854d91ee0a138742013
Bill Turvill <turvill@hotmail.co.uk>
2023-02-06 16:30:11 +0000

    html

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
+<?php
+require_once('functions.php');
+require_once('db.php');
+?>
+
 <html>
     <head>
         <title>Mr Client's Antique Collection</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+        <!--<link rel="stylesheet" type="text/css" href="style.css">-->
+        <!--<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">-->
 </head>
     <body>
-        <h1>Mr Client's Antique Collection</h1>
-        <p>Hello, welcome to my antique collection.<p>
+        <header class="header">
+            <h1>Mr Client's Antique Collection</h1>
+            <p>Hello, welcome to my antique collection.<p>
+</header>
+        <div>
+        <?=printPlease($antiques);?>
+</div>
         <!--call function-->
    </body>
 </html>
